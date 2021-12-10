1bb7004d8c1332f58a4ea88f74d5f8ea85b0b9ca
Esaak <gerasimov.ia@phystech.edu>
2021-12-11 00:01:55 +0300

    11.12

---
+++ matrix/cmake-build-debug/CMakeFiles/matrix_musor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrix_musor.dir/musor.cpp.obj"
+  "libmatrix_musor.dll.a"
+  "matrix_musor.exe"
+  "matrix_musor.exe.manifest"
+  "matrix_musor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matrix_musor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
